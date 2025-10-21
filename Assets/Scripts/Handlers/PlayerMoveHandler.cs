fcc0742cb0e20d91b776025b6b5396fee8089fbf
covmoore <covmoore5@gmail.com>
2025-10-20 20:30:06 -0700

    adding bootstaper and gun noise range

--- Assets/Scripts/Handlers/PlayerMoveHandler.cs
+++ Assets/Scripts/Handlers/PlayerMoveHandler.cs
@@ -5,96 +5,96 @@ using NUnit.Framework;
 
 namespace RaveSurvival
 {
-    public class PlayerMoveHandler : NetworkBehaviour
+  public class PlayerMoveHandler : NetworkBehaviour
+  {
+    // Reference to the CharacterController component for handling movement
+    public CharacterController controller;
+
+    // Movement speed of the player
+    public float speed = 12f;
+
+    // Gravity applied to the player
+    public float gravity = -9.81f;
+
+    // Height the player can jump
+    public float jumpHeight = 3f;
+
+    // Transform used to check if the player is grounded
+    public Transform groundCheck;
+
+    // Radius of the sphere used for ground detection
+    public float groundDistance = 0.4f;
+
+    // Layer mask to identify what is considered ground
+    public LayerMask groundMask;
+
+    // Velocity vector for vertical movement (gravity and jumping)
+    Vector3 velocity;
+
+    // Boolean to check if the player is grounded
+    bool isGrounded;
+
+    /// <summary>
+    /// Unity's Update method, called once per frame.
+    /// Handles player movement and jumping for the local player.
+    /// </summary>
+    void Update()
     {
-        // Reference to the CharacterController component for handling movement
-        public CharacterController controller;
+      if (GameManager.Instance == null)
+      {
+        Debug.LogError("Error... GameManager is null when trying to move player...");
+        return;
+      }
 
-        // Movement speed of the player
-        public float speed = 12f;
-
-        // Gravity applied to the player
-        public float gravity = -9.81f;
-
-        // Height the player can jump
-        public float jumpHeight = 3f;
-
-        // Transform used to check if the player is grounded
-        public Transform groundCheck;
-
-        // Radius of the sphere used for ground detection
-        public float groundDistance = 0.4f;
-
-        // Layer mask to identify what is considered ground
-        public LayerMask groundMask;
-
-        // Velocity vector for vertical movement (gravity and jumping)
-        Vector3 velocity;
-
-        // Boolean to check if the player is grounded
-        bool isGrounded;
-
-        /// <summary>
-        /// Unity's Update method, called once per frame.
-        /// Handles player movement and jumping for the local player.
-        /// </summary>
-        void Update()
+      if (GameManager.Instance.gameType == GameManager.GameType.OnlineMultiplayer)
+      {
+        // Ensure the movement logic only applies to the local player
+        if (isLocalPlayer)
         {
-            if (GameManager.Instance == null)
-            {
-                Debug.LogError("Error... GameManager is null when trying to move player...");
-                return;
-            }
-
-            if (GameManager.Instance.gameType == GameManager.GameType.OnlineMultiplayer)
-            {
-                // Ensure the movement logic only applies to the local player
-                if (isLocalPlayer)
-                {
-                    PlayerMove();
-                }
-            }
-            else if (GameManager.Instance.gameType == GameManager.GameType.SinglePlayer)
-            {
-                PlayerMove();
-            }
-            
+          PlayerMove();
         }
-
-        void PlayerMove()
-        {
-            // Check if the player is grounded using a sphere at the groundCheck position
-            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-            // Reset vertical velocity if the player is grounded
-            if (isGrounded && velocity.y < 0)
-            {
-                velocity.y = -2f; // Small negative value to keep the player grounded
-            }
-
-            // Get input for horizontal (x) and vertical (z) movement
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-
-            // Calculate the movement direction based on input and player orientation
-            Vector3 move = transform.right * x + transform.forward * z;
-
-            // Move the player using the CharacterController
-            controller.Move(move * speed * Time.deltaTime);
-
-            // Check if the jump button is pressed and the player is grounded
-            if (Input.GetButtonDown("Jump") && isGrounded)
-            {
-                // Calculate the vertical velocity needed to achieve the desired jump height
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
-
-            // Apply gravity to the vertical velocity
-            velocity.y += gravity * Time.deltaTime;
-
-            // Apply the vertical velocity to the player
-            controller.Move(velocity * Time.deltaTime);
-        }
+      }
+      else if (GameManager.Instance.gameType == GameManager.GameType.SinglePlayer)
+      {
+        PlayerMove();
+      }
 
     }
+
+    void PlayerMove()
+    {
+      // Check if the player is grounded using a sphere at the groundCheck position
+      isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+      // Reset vertical velocity if the player is grounded
+      if (isGrounded && velocity.y < 0)
+      {
+        velocity.y = -2f; // Small negative value to keep the player grounded
+      }
+
+      // Get input for horizontal (x) and vertical (z) movement
+      float x = Input.GetAxis("Horizontal");
+      float z = Input.GetAxis("Vertical");
+
+      // Calculate the movement direction based on input and player orientation
+      Vector3 move = transform.right * x + transform.forward * z;
+
+      // Move the player using the CharacterController
+      controller.Move(move * speed * Time.deltaTime);
+
+      // Check if the jump button is pressed and the player is grounded
+      if (Input.GetButtonDown("Jump") && isGrounded)
+      {
+        // Calculate the vertical velocity needed to achieve the desired jump height
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+      }
+
+      // Apply gravity to the vertical velocity
+      velocity.y += gravity * Time.deltaTime;
+
+      // Apply the vertical velocity to the player
+      controller.Move(velocity * Time.deltaTime);
+    }
+
+  }
 }
--- Assets/Scripts/Models/Gun.cs
+++ Assets/Scripts/Models/Gun.cs
@@ -5,278 +5,280 @@ using UnityEngine.SocialPlatforms;
 
 public class Gun : NetworkBehaviour
 {
-	public float damage = 10f;
-	public float range = 100f;
-	public float fireRate = 15f;
-	private float nextTimeToFire = 0f;
+  public float damage = 10f;
+  public float range = 100f;
+  public float fireRate = 15f;
+  public float soundRange = 100f;
+  private float nextTimeToFire = 0f;
 
-	public WeaponType weaponType = WeaponType.RAYCAST;
+  public WeaponType weaponType = WeaponType.RAYCAST;
 
-	[SerializeField]
-	public Transform bulletStart;
-	public ParticleSystem muzzleFlash;
-	public GameObject impactEffect;
+  [SerializeField]
+  public Transform bulletStart;
+  public ParticleSystem muzzleFlash;
+  public GameObject impactEffect;
 
-	public GameObject projectile;
+  public GameObject projectile;
 
-	private AudioSource audioSource;
-	public AudioClip fireSound;
-	public enum WeaponType
-	{
-		RAYCAST = 0,
-		PROJECTILE
-	}
+  private AudioSource audioSource;
+  public AudioClip fireSound;
+  public enum WeaponType
+  {
+    RAYCAST = 0,
+    PROJECTILE
+  }
 
-	private bool isReady = false;
+  private bool isReady = false;
 
-	void Awake()
-	{
-		if (this.weaponType == WeaponType.PROJECTILE && bulletStart == null)
-		{
-			bulletStart = transform.Find("bulletSpawn");
-			if (bulletStart == null)
-			{
-				Debug.LogError("Gun: bulletSpawn Transform not found on this weapon!");
-			}
-		}
+  void Awake()
+  {
+    if (this.weaponType == WeaponType.PROJECTILE && bulletStart == null)
+    {
+      bulletStart = transform.Find("bulletSpawn");
+      if (bulletStart == null)
+      {
+        Debug.LogError("Gun: bulletSpawn Transform not found on this weapon!");
+      }
+    }
 
-		audioSource = GetComponent<AudioSource>();
-	}
+    audioSource = GetComponent<AudioSource>();
+  }
 
 
-	public override void OnStartLocalPlayer()
-	{
-		base.OnStartLocalPlayer();
-		isReady = true;
-	}
+  public override void OnStartLocalPlayer()
+  {
+    base.OnStartLocalPlayer();
+    isReady = true;
+  }
 
-	void Start()
-	{
-		isReady = true;
-	}
+  void Start()
+  {
+    isReady = true;
+  }
 
-	// Update is called once per frame
-	void Update()
-	{
+  // Update is called once per frame
+  void Update()
+  {
 
-		if (GameManager.Instance == null)
-		{
-			Debug.LogError("Error... Game manager instance is null when trying to shoot!");
-			return;
-		}
+    if (GameManager.Instance == null)
+    {
+      Debug.LogError("Error... Game manager instance is null when trying to shoot!");
+      return;
+    }
 
-		if (!isReady)
-		{
-			return;
-		}
+    if (!isReady)
+    {
+      return;
+    }
 
-		if (GameManager.Instance.gameType == GameManager.GameType.OnlineMultiplayer && !isLocalPlayer)
-		{
-			return;
-		}
+    if (GameManager.Instance.gameType == GameManager.GameType.OnlineMultiplayer && !isLocalPlayer)
+    {
+      return;
+    }
 
-		if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
-		{
-			nextTimeToFire = Time.time + (1f / fireRate);
-			if (GameManager.Instance.gameType == GameManager.GameType.OnlineMultiplayer)
-			{
-				OnlineShoot(false);
-			}
-			else if (GameManager.Instance.gameType == GameManager.GameType.SinglePlayer)
-			{
-				if (LayerMask.LayerToName(this.gameObject.layer) == "Player")
-				{
-					SinglePlayerShoot(false);
-				}
-			}
-		}
-	}
+    if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+    {
+      nextTimeToFire = Time.time + (1f / fireRate);
+      if (GameManager.Instance.gameType == GameManager.GameType.OnlineMultiplayer)
+      {
+        OnlineShoot(false);
+      }
+      else if (GameManager.Instance.gameType == GameManager.GameType.SinglePlayer)
+      {
+        if (LayerMask.LayerToName(this.gameObject.layer) == "Player")
+        {
+          SinglePlayerShoot(false);
+          this.gameObject.GetComponentInParent<Player>().AlertNearEnemies();
+        }
+      }
+    }
+  }
 
-	public void SetBulletStart(Transform start)
-	{
-		bulletStart = start;
-	}
+  public void SetBulletStart(Transform start)
+  {
+    bulletStart = start;
+  }
 
-	public void OnlineShoot(bool isEnemy)
-	{
-		if (bulletStart == null)
-		{
-			Debug.LogWarning("bulletStart is null, cannot shoot");
-			return;
-		}
+  public void OnlineShoot(bool isEnemy)
+  {
+    if (bulletStart == null)
+    {
+      Debug.LogWarning("bulletStart is null, cannot shoot");
+      return;
+    }
 
-		// Get the origin and direction of the shot from the camera
-		Vector3 originPosition = bulletStart.position;
-		Vector3 direction = bulletStart.forward;
+    // Get the origin and direction of the shot from the camera
+    Vector3 originPosition = bulletStart.position;
+    Vector3 direction = bulletStart.forward;
 
-		if (isEnemy)
-		{
-			if (!isServer)
-			{
-				Debug.LogWarning("Enemy tried to shoot but not on the server");
-				return;
-			}
-			if (Time.time < nextTimeToFire)
-			{
-				return;
-			}
-			nextTimeToFire = Time.time + (1f / fireRate);
-			ServerShoot(originPosition, direction);
-			return;
-		}
+    if (isEnemy)
+    {
+      if (!isServer)
+      {
+        Debug.LogWarning("Enemy tried to shoot but not on the server");
+        return;
+      }
+      if (Time.time < nextTimeToFire)
+      {
+        return;
+      }
+      nextTimeToFire = Time.time + (1f / fireRate);
+      ServerShoot(originPosition, direction);
+      return;
+    }
 
-		if (isLocalPlayer)
-		{
-			muzzleFlash.Play();
-			audioSource.Play();
-		}
+    if (isLocalPlayer)
+    {
+      muzzleFlash.Play();
+      audioSource.Play();
+    }
 
-		if (!isServer)
-		{
-			CmdShoot(originPosition, direction);
-		}
-		else
-		{
-			//fallback case: host shooting
-			ServerShoot(originPosition, direction);
-		}
-	}
+    if (!isServer)
+    {
+      CmdShoot(originPosition, direction);
+    }
+    else
+    {
+      //fallback case: host shooting
+      ServerShoot(originPosition, direction);
+    }
+  }
 
-	public void SinglePlayerShoot(bool isEnemy)
-	{
-		if (bulletStart == null)
-		{
-			Debug.LogWarning("bulletStart is null, cannot shoot");
-			return;
-		}
+  public void SinglePlayerShoot(bool isEnemy)
+  {
+    if (bulletStart == null)
+    {
+      Debug.LogWarning("bulletStart is null, cannot shoot");
+      return;
+    }
 
-		// Get the origin and direction of the shot from the camera
-		Vector3 originPosition = bulletStart.position;
-		Vector3 direction = bulletStart.forward;
+    // Get the origin and direction of the shot from the camera
+    Vector3 originPosition = bulletStart.position;
+    Vector3 direction = bulletStart.forward;
 
-		if (isEnemy)
-		{
-			if (Time.time < nextTimeToFire)
-			{
-				return;
-			}
-			nextTimeToFire = Time.time + (1f / fireRate);
-			LocalShoot(originPosition, direction);
-			return;
-		}
-		else
-		{
-			LocalShoot(originPosition, direction);
-		}
-		
-	}
+    if (isEnemy)
+    {
+      if (Time.time < nextTimeToFire)
+      {
+        return;
+      }
+      nextTimeToFire = Time.time + (1f / fireRate);
+      LocalShoot(originPosition, direction);
+      return;
+    }
+    else
+    {
+      LocalShoot(originPosition, direction);
+    }
 
-	void LocalShoot(Vector3 originPosition, Vector3 direction)
-	{
-		PlayMuzzleFlash();
-		if (weaponType == WeaponType.RAYCAST)
-		{
+  }
 
-			RaycastHit hit;
-			if (Physics.Raycast(originPosition, direction, out hit, range))
-			{
-				Enemy enemy = hit.transform.GetComponent<Enemy>();
-				if (enemy != null)
-				{
-					enemy.TakeDamage(damage, bulletStart, originPosition);
-				}
+  void LocalShoot(Vector3 originPosition, Vector3 direction)
+  {
+    PlayMuzzleFlash();
+    if (weaponType == WeaponType.RAYCAST)
+    {
 
-				GameObject impactFx = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-				Destroy(impactFx, 2f);
-			}
-		}
-		else if (weaponType == WeaponType.PROJECTILE)
-		{
-			GameObject projectile = Instantiate(this.projectile, originPosition, Quaternion.LookRotation(direction));
-			projectile.GetComponent<Projectile>().FireBullet(15f);
-		}
-	}
+      RaycastHit hit;
+      if (Physics.Raycast(originPosition, direction, out hit, range))
+      {
+        Enemy enemy = hit.transform.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+          enemy.TakeDamage(damage, bulletStart, originPosition);
+        }
 
-	[Server]
-	void ServerShoot(Vector3 originPosition, Vector3 direction)
-	{
+        GameObject impactFx = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactFx, 2f);
+      }
+    }
+    else if (weaponType == WeaponType.PROJECTILE)
+    {
+      GameObject projectile = Instantiate(this.projectile, originPosition, Quaternion.LookRotation(direction));
+      projectile.GetComponent<Projectile>().FireBullet(15f);
+    }
+  }
 
-		RpcPlayMuzzleFlash();
+  [Server]
+  void ServerShoot(Vector3 originPosition, Vector3 direction)
+  {
 
-		if (weaponType == WeaponType.RAYCAST)
-		{
+    RpcPlayMuzzleFlash();
 
-			RaycastHit hit;
-			if (Physics.Raycast(originPosition, direction, out hit, range))
-			{
-				Enemy enemy = hit.transform.GetComponent<Enemy>();
-				if (enemy != null)
-				{
-					enemy.TakeDamage(damage, bulletStart, originPosition);
-				}
+    if (weaponType == WeaponType.RAYCAST)
+    {
 
-				GameObject impactFx = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-				Destroy(impactFx, 2f);
-			}
-		}
-		else if (weaponType == WeaponType.PROJECTILE)
-		{
-			GameObject projectile = Instantiate(this.projectile, originPosition, Quaternion.LookRotation(direction));
-			NetworkServer.Spawn(projectile);
-			projectile.GetComponent<Projectile>().FireBullet(15f);
-		}
-	}
+      RaycastHit hit;
+      if (Physics.Raycast(originPosition, direction, out hit, range))
+      {
+        Enemy enemy = hit.transform.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+          enemy.TakeDamage(damage, bulletStart, originPosition);
+        }
 
-	[Command]
-	void CmdShoot(Vector3 originPosition, Vector3 direction)
-	{
-		RpcPlayMuzzleFlash();
-		if (weaponType == WeaponType.RAYCAST)
-		{
+        GameObject impactFx = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactFx, 2f);
+      }
+    }
+    else if (weaponType == WeaponType.PROJECTILE)
+    {
+      GameObject projectile = Instantiate(this.projectile, originPosition, Quaternion.LookRotation(direction));
+      NetworkServer.Spawn(projectile);
+      projectile.GetComponent<Projectile>().FireBullet(15f);
+    }
+  }
 
-			RaycastHit hit;
-			if (Physics.Raycast(originPosition, direction, out hit, range))
-			{
-				Enemy enemy = hit.transform.GetComponent<Enemy>();
-				if (enemy != null)
-				{
-					enemy.TakeDamage(damage, bulletStart, originPosition);
-				}
+  [Command]
+  void CmdShoot(Vector3 originPosition, Vector3 direction)
+  {
+    RpcPlayMuzzleFlash();
+    if (weaponType == WeaponType.RAYCAST)
+    {
 
-				GameObject impactFx = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-				Destroy(impactFx, 2f);
-			}
-		}
-		else if (weaponType == WeaponType.PROJECTILE)
-		{
-			GameObject projectile = Instantiate(this.projectile, originPosition, Quaternion.LookRotation(direction));
-			NetworkServer.Spawn(projectile);
-			projectile.GetComponent<Projectile>().FireBullet(15f);
-		}
-	}
+      RaycastHit hit;
+      if (Physics.Raycast(originPosition, direction, out hit, range))
+      {
+        Enemy enemy = hit.transform.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+          enemy.TakeDamage(damage, bulletStart, originPosition);
+        }
 
-	/// <summary>
-	/// ClientRpc method executed on all clients to play the muzzle flash effect.
-	/// Ensures the effect is played for non-local players.
-	/// </summary>
-	[ClientRpc]
-	void RpcPlayMuzzleFlash()
-	{
-		if (audioSource.clip == null || audioSource.clip != fireSound)
-		{
-			audioSource.clip = fireSound;
-		}
-		audioSource.Play();
-		muzzleFlash.Play();
-	}
+        GameObject impactFx = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactFx, 2f);
+      }
+    }
+    else if (weaponType == WeaponType.PROJECTILE)
+    {
+      GameObject projectile = Instantiate(this.projectile, originPosition, Quaternion.LookRotation(direction));
+      NetworkServer.Spawn(projectile);
+      projectile.GetComponent<Projectile>().FireBullet(15f);
+    }
+  }
 
-	void PlayMuzzleFlash()
-	{
-		if (audioSource.clip == null || audioSource.clip != fireSound)
-		{
-			audioSource.clip = fireSound;
-		}
-		audioSource.Play();
-		muzzleFlash.Play();
-	}
+  /// <summary>
+  /// ClientRpc method executed on all clients to play the muzzle flash effect.
+  /// Ensures the effect is played for non-local players.
+  /// </summary>
+  [ClientRpc]
+  void RpcPlayMuzzleFlash()
+  {
+    if (audioSource.clip == null || audioSource.clip != fireSound)
+    {
+      audioSource.clip = fireSound;
+    }
+    audioSource.Play();
+    muzzleFlash.Play();
+  }
+
+  void PlayMuzzleFlash()
+  {
+    if (audioSource.clip == null || audioSource.clip != fireSound)
+    {
+      audioSource.clip = fireSound;
+    }
+    audioSource.Play();
+    muzzleFlash.Play();
+  }
 }
